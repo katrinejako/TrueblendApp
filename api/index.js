b806006f730590b6a02c375e418593bcd3c062e1
Oyelaja Hussein <premiumitng@gmail.com>
2025-03-03 10:44:49 +0100

    Updated the default API port

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.get("/", (req, res) => {
   res.send("Welcome to TrueblendApp API");
