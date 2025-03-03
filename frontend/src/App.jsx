7fa4758a456042a5566adbdcc91c6849893194c1
Oyelaja Hussein <premiumitng@gmail.com>
2025-03-03 10:54:10 +0100

    Installed Tailwind CSS for styling

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className='bg-blue-500 text-white p-4'>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
